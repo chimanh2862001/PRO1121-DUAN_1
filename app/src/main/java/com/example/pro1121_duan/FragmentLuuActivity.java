e0a7cf95ccf37aedcf344c72bd1a82103c0a5b5c
dungdtph11119 <dungdtph11119@fpt.edu.vn>
2020-11-05 11:54:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pro1121_duan/FragmentLuuActivity.java
@@ -0,0 +1,29 @@
+package com.example.pro1121_duan;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentLuuActivity extends Fragment {
+
+
+
+    public FragmentLuuActivity() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_luu_activity, container, false);;
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/pro1121_duan/FragmentTimKiemActivity.java
@@ -0,0 +1,29 @@
+package com.example.pro1121_duan;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentTimKiemActivity extends Fragment {
+
+
+
+    public FragmentTimKiemActivity() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_tim_kiem_activity, container, false);
+        return  view;
+    }
+}
